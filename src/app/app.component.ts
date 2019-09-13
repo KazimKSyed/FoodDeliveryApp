7154a6a8ee28bdaebda2eec3d95b058b0ab599a3
KazimKSyed <syedkazim56@gmail.com>
2019-09-13 16:48:15 +0530

    Template adde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'FoodDeliveryApp';
+
+  constructor(
+private elementRef:ElementRef
+  )
+  {}
+  ngOnInit(){}
+
+  ngAfterViewInit(){
+    var style = document.createElement('link');
+    style.type = 'text/css';
+    style.href='./assets/css/style.css';
+    style.rel='stylesheet';
+    this.elementRef.nativeElement.appendChild(style);
+  }
 }
